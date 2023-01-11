78e2855a8fe8dfda7e7693840c87a51875168d2c
Vrushal Patel <vrushalkpatel007@gmail.com>
2023-01-11 14:29:09 +0530

    added functionality in CartReducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,8 +8,18 @@ const defaultCartState = {
 
 const cartReducer = (state,action) => {
     if(action.type==='ADD_ITEM') {
-        const updatedItems = state.items.concat(action.item)
+        console.log(state.items)
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        let updatedItems;
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id )
+        const existingCartItem = state.items[existingCartItemIndex]
+        if(existingCartItem){
+            let updatedItem = {...existingCartItem,amount:existingCartItem.amount + action.item.amount}
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem
+        }else{
+            updatedItems = state.items.concat(action.item)
+        }
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
